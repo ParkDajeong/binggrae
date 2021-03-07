c3b3578d16efa1d2c25e675e8c9e76896575f953
ParkDajeong <sobeast980@gmail.com>
2021-03-08 08:16:42 +0900

    mobile nav js 구현

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-$(function() {  
+$(function() {
+  // PC Nav Slide Event
   $(".gnb__menu-item, .gnb__2depth-area").hover(function() {
     $(".gnb__sub-menu, .gnb__2depth-area, .gnb__bg").stop().slideDown();
   }, function() {
     $(".gnb__sub-menu, .gnb__2depth-area, .gnb__bg").stop().slideUp();
+  });
+
+  // Mobile Nav Toggle
+  $(".btn-toggle-menu").on("click", function() {
+    $(this).toggleClass("on");
+    $(".gnb-mo, .header__logo").toggleClass("on");
+  });
+
+  $(".gnb-mo__link--dropdown").on("click", function(e) {
+    e.preventDefault();
+    $(this).toggleClass("on");
   });
 });
