9b6b78e1caae0e91a26aaaa947b7058fd3af70dd
ParkDajeong <sobeast980@gmail.com>
2021-03-07 11:19:19 +0900

    header 반응형 일부 구현

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {  
-  $(".gnb__menu-item").hover(function() {
-    $(".gnb__sub-menu").stop().slideDown(400);
+  $(".gnb__menu-item, .gnb__2depth-area").hover(function() {
+    $(".gnb__sub-menu, .gnb__2depth-area, .gnb__bg").stop().slideDown();
   }, function() {
-    $(".gnb__sub-menu").stop().slideUp(400);
+    $(".gnb__sub-menu, .gnb__2depth-area, .gnb__bg").stop().slideUp();
   });
 });
